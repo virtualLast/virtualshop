b8bbee2de7d79b0f6bce7a065484a2efee9d85b8
virtualLast <mjlast@gmail.com>
2022-12-07 13:40:06 +0000

    added sidebar button toggle

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,5 +1,8 @@
 <template>
-    <div :class="[$style.sidebar, 'p-3', 'mb-5']">
+    <div
+        :class="[$style.sidebar, 'p-3', 'mb-5']"
+        :style="{ width: collapsed ? '70px' : 'auto' }"
+    >
         <h5 class="text-center">
             Categories
         </h5>
@@ -24,6 +27,13 @@
                 </a>
             </li>
         </ul>
+        <div class="d-flex justify-content-end">
+            <button
+                class="btn btn-secondary btn-sm"
+                @click="toggleCollapsed"
+                v-text="collapsed ? '>>' : '<< Collapse'"
+            />
+        </div>
     </div>
 </template>
 
@@ -33,6 +43,7 @@ export default {
     name: 'Sidebar',
     data() {
         return {
+            collapsed: false,
             categories: [
                 {
                     name: 'Dot Matrix Printers',
@@ -44,6 +55,11 @@ export default {
                 },
             ],
         };
+    },
+    methods: {
+        toggleCollapsed() {
+            this.collapsed = !this.collapsed;
+        },
     },
 };
 </script>
