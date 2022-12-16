f337c137b321ff2e919e35e0703fcd44dee224c9
virtualLast <mjlast@gmail.com>
2022-12-16 09:49:54 +0000

    added debouncing to search bar

--- assets/js/components/search-bar.vue
+++ assets/js/components/search-bar.vue
@@ -17,11 +17,18 @@ export default {
     data() {
         return {
             searchTerm: '',
+            searchTimeout: null,
         };
     },
     methods: {
         onInput() {
-            this.$emit('search-products', { term: this.searchTerm });
+            if (this.searchTimeout) {
+                clearTimeout(this.searchTimeout);
+            }
+            this.searchTimeout = setTimeout(() => {
+                this.$emit('search-products', { term: this.searchTerm });
+                this.searchTimeout = null;
+            }, 200);
         },
     },
 };
